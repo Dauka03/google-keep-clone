ffa8450541d40c1c76d6dd1c816fd61ab673b7bc
Dauka03 <dauka0300@gmail.com>
2022-09-04 12:13:23 +0600

    first commit

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,44 @@
+
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Menu } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+import * as React from "react";
+
+const Header = styled(AppBar)`
+  z-index: 1201;
+  background: #fff;
+  height: 70px;
+  box-shadow: inset 0 -1px 0 0 #dadce0;
+`
+
+const Heading = styled(Typography)`
+  color: #5F6368;
+  font-size: 24px;
+  margin-left: 25px;
+`
+export interface HeaderProps {
+  open: boolean
+}
+
+
+const HeaderBar = ({open ,handleDrawer}) => {
+  const logo = 'https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png';
+  
+  return (
+    <Header>
+      <Toolbar>
+        <IconButton
+          onClick={() => handleDrawer()}
+          sx={{ marginRight: '20px'}}
+          edge="start"
+        >
+          <Menu />
+        </IconButton>
+        <img src={logo} alt="logo" style={{width: 30}} />
+        <Heading>Keep</Heading>
+      </Toolbar>
+    </Header>
+  )
+}
+
+export default HeaderBar;
